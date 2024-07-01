dcad6e01b6d785ad1c609ce7fcf2ae7b3fa04646
Sergey Petrochenko <proffsneip@gmail.com>
2024-07-01 20:28:51 +0300

    producer

---
+++ src/producer/KP.Worker/Generator/WeatherGenerator.cs
@@ -0,0 +1,20 @@
+﻿using KP.Worker.Models;
+
+namespace KP.Worker.Generator;
+
+internal static class WeatherGenerator
+{
+    private static string[] Cities => new[] { "Moscow", "New York", "London", "Minsk", "Mogilev", "Warsaw" }; 
+    
+    public static IEnumerable<WeatherForecast> Generate()
+    {
+        var randomStart = new Random().Next(1, 10);
+        var randomCount = new Random().Next(1, 10);
+        var cityIndex = new Random().Next(0, Cities.Length);
+        
+        var date = DateTime.UtcNow;
+
+        return Enumerable.Range(randomStart, randomCount)
+            .Select(item => new WeatherForecast(Cities[cityIndex], item * 11 ,date));
+    }
+}
---
+++ src/producer/KP.Worker/Models/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿namespace KP.Worker.Models;
+
+internal record WeatherForecast
+{
+    public DateTime Date { get; }
+    
+    public string City { get; }
+
+    public double TemperatureInCelsius { get; }
+
+    public double TemperatureInFahrenheit => TemperatureInCelsius * 1.8 + 32;
+
+    public WeatherForecast(string city, double temperatureInCelsius, DateTime date)
+    {
+        City = city;
+        TemperatureInCelsius = temperatureInCelsius;
+        Date = date;
+    }
+}
